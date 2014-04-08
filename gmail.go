7b5d29e28189afe0de1b2f535aaed6c6327d3b7b
Martin Bruse <zondolfin@gmail.com>
2014-04-08 18:55:35 +0200

    made it simple to use an alternative SMTP host

--- gmail.go
+++ gmail.go
@@ -6,11 +6,16 @@ import (
 	"net/smtp"
 	"regexp"
 	"strings"
+
 	"code.google.com/p/mahonia"
 
 	"github.com/jhillyerd/go.enmime"
 	"github.com/zond/gmail/imap"
 	"github.com/zond/gmail/xmpp"
+)
+
+const (
+	GMailSMTP = "smtp.gmail.com:587"
 )
 
 func DecodeText(body, mimeContent string) string {
@@ -36,6 +41,9 @@ type Client struct {
 	imapClient   *imap.Client
 	mailHandler  imap.MailHandler
 	errorHandler func(e error)
+	smtpHost     string
+	smtpAccount  string
+	smtpPassword string
 }
 
 func New(account, password string) (result *Client) {
@@ -51,6 +59,9 @@ func New(account, password string) (result *Client) {
 		errorHandler: func(e error) {
 			fmt.Println("Error", e)
 		},
+		smtpHost:     GMailSMTP,
+		smtpAccount:  account,
+		smtpPassword: password,
 	}
 	result.xmppClient.MailHandler(func() {
 		if err := result.imapClient.HandleNew(result.mailHandler); err != nil {
@@ -66,18 +77,28 @@ var AddrReg = regexp.MustCompile("(?i)[=A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}")
 
 func (self *Client) Send(from, subject, message string, recips ...string) (err error) {
 	body := fmt.Sprintf("Content-Type: text/plain; charset=\"utf-8\"\r\nReply-To: %v\r\nFrom: %v\r\nTo: %v\r\nSubject: %v\r\n\r\n%v", from, from, strings.Join(recips, ", "), subject, message)
-	auth := smtp.PlainAuth("", self.account, self.password, "smtp.gmail.com")
+	var auth smtp.Auth
+	if self.smtpAccount != "" {
+		auth = smtp.PlainAuth("", self.smtpAccount, self.smtpPassword, strings.Split(self.smtpHost, ":")[0])
+	}
 	actualRecips := []string{}
 	for _, recip := range recips {
 		if match := AddrReg.FindString(recip); match != "" {
 			actualRecips = append(actualRecips, match)
 		}
 	}
-	return smtp.SendMail("smtp.gmail.com:587", auth, self.account, actualRecips, []byte(body))
+	return smtp.SendMail(self.smtpHost, auth, self.account, actualRecips, []byte(body))
 }
 
 func (self *Client) Debug() *Client {
 	self.xmppClient.Debug()
+	return self
+}
+
+func (self *Client) SMTPHost(host, account, password string) *Client {
+	self.smtpHost = host
+	self.smtpAccount = account
+	self.smtpPassword = password
 	return self
 }
 
